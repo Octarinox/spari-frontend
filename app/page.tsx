b97665f1f2ec730a415a05818ee7964966c78a27
N0do <n0doprods@gmail.com>
2023-09-26 22:25:47 +0400

    project initialization

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Search from "@/components/Search";
+
+export default async function IndexPage() {
+   return (
+      <main className="p-4 md:p-10 mx-auto max-w-7xl">
+         <Search />
+      </main>
+   );
+}
