3697e6e8e9595ba894ee168de75906e798c3a2fa
davit <kiladzedato5@gmail.com>
2023-11-06 23:17:32 +0400

    feat: dynamic generation of image for icon

--- constants/settingsMenuItems.tsx
+++ constants/settingsMenuItems.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 import styles from "@/shared/active.module.css";
 
+const activeClass =
+   "flex items-center p-2 hover:text-gray-900 rounded-lg bg-gray-200 group";
+const inactiveClass =
+   "flex items-center p-2 text-gray-500 hover:text-gray-900 rounded-lg  hover:bg-gray-100 group";
+const generateImageTag = (icon: string, alt: string, styles: any) => {
+   return (
+      <img
+         src={icon}
+         width={"20px"}
+         height={"20px"}
+         alt={alt}
+         className={styles}
+      />
+   );
+};
+
 export const settingsMenuItems = [
    {
       text: "User Register",
@@ -8,22 +24,16 @@ export const settingsMenuItems = [
          const activeItemStyle = this.current
             ? styles.activeIcon
             : styles.inactiveIcon;
-         return (
-            <img
-               src="/users.svg"
-               width={"20px"}
-               height={"20px"}
-               alt={"user_register_icon"}
-               className={activeItemStyle}
-            />
+         return generateImageTag(
+            "/users.svg",
+            "user_register_icon",
+            activeItemStyle
          );
       },
       href: "/settings/user-register",
       current: false,
-      className:
-         "flex items-center p-2 text-gray-500 hover:text-gray-900 rounded-lg hover:bg-gray-100 group",
-      classNameCurrent:
-         "flex items-center p-2 hover:text-gray-900 rounded-lg bg-gray-200 group",
+      className: inactiveClass,
+      classNameCurrent: activeClass,
    },
    {
       text: "Branch Register",
@@ -31,22 +41,16 @@ export const settingsMenuItems = [
          const activeItemStyle = this.current
             ? styles.activeIcon
             : styles.inactiveIcon;
-         return (
-            <img
-               src="/branch.svg"
-               width={"20px"}
-               height={"20px"}
-               alt={"branch_register_icon"}
-               className={activeItemStyle}
-            />
+         return generateImageTag(
+            "/branch.svg",
+            "branch_register_icon",
+            activeItemStyle
          );
       },
       href: "/settings/branch-register",
       current: false,
-      className:
-         "flex items-center p-2 text-gray-500 hover:text-gray-900 rounded-lg  hover:bg-gray-100 group",
-      classNameCurrent:
-         "flex items-center p-2 hover:text-gray-900 rounded-lg bg-gray-200 group",
+      className: inactiveClass,
+      classNameCurrent: activeClass,
    },
    {
       text: "Blacklist",
@@ -55,21 +59,15 @@ export const settingsMenuItems = [
          const activeItemStyle = this.current
             ? styles.activeIcon
             : styles.inactiveIcon;
-         return (
-            <img
-               src="/blacklist.svg"
-               width={"20px"}
-               height={"20px"}
-               alt={"blacklist_icon"}
-               className={activeItemStyle}
-            />
+         return generateImageTag(
+            "/blacklist.svg",
+            "blacklist_icon",
+            activeItemStyle
          );
       },
       href: "/settings/blacklist",
       current: false,
-      className:
-         "flex items-center p-2 text-gray-500 hover:text-gray-900 rounded-lg  hover:bg-gray-100 group",
-      classNameCurrent:
-         "flex items-center p-2 hover:text-gray-900 rounded-lg bg-gray-200 group",
+      className: inactiveClass,
+      classNameCurrent: activeClass,
    },
 ];
