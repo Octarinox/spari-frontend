42e9da5290192c32406d984fd8eaa4fd18fee90f
davit <kiladzedato5@gmail.com>
2023-12-04 00:18:21 +0400

    refactor: changed endpoints

--- app/next-api/login/route.ts
+++ app/next-api/login/route.ts
@@ -2,14 +2,17 @@ export async function POST(request: Request) {
    const req = await request.json();
 
    try {
-      const response = await fetch("https://octarinox.tech/api/user/login", {
-         method: "POST",
-         body: JSON.stringify({ email: req.email, password: req.password }),
-         headers: {
-            "Content-Type": "application/json",
-         },
-         credentials: "include",
-      });
+      const response = await fetch(
+         "https://octarinox.tech/api/auth/user/login",
+         {
+            method: "POST",
+            body: JSON.stringify({ email: req.email, password: req.password }),
+            headers: {
+               "Content-Type": "application/json",
+            },
+            credentials: "include",
+         }
+      );
 
       if (!response.ok) {
          throw new Error("network response was not OK");
--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(request: NextRequest) {
    const token = request.cookies.get("jwt");
 
    const res = await fetch(
-      new URL("https://octarinox.tech/api/user/check-auth").href,
+      new URL("https://octarinox.tech/api/auth/user/check-auth").href,
       {
          method: "GET",
          headers: {
