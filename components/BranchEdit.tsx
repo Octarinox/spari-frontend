af1b38cdf93c0fd2d5dd9d8bc333571845b9aafc
NonoPir <sanoonas100@gmail.com>
2023-12-10 11:59:02 +0400

    Implemented filter component

--- components/BranchEdit.tsx
+++ components/BranchEdit.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React from "react";
+import SearchBarComponent from "./UI Components/FilterComponent";
+import { BranchesSelectComponent } from "./UI Components/FilterComponent";
 
 interface Column {
    id: "branchID" | "address";
@@ -39,6 +41,10 @@ const BranchEditTable = () => {
       <div>
          <div className=" flex justify-center">
             <h1 className="text-3xl font-bold text-gray-700">Branch List</h1>
+         </div>
+         <div className="flex w-4/4 justify-end">
+            <SearchBarComponent />
+            <BranchesSelectComponent />
          </div>
       </div>
    );
