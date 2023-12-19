e83976668d7bd0169939f1fe64e5cdd5cc374e4b
davit <kiladzedato5@gmail.com>
2023-12-20 01:57:58 +0400

    feat: check permission on facerec

--- app/(loggedIn)/(branchPage)/branch/[id]/edit-branch/page.tsx
+++ app/(loggedIn)/(branchPage)/branch/[id]/edit-branch/page.tsx
@@ -17,7 +17,7 @@ const EditBranch = () => {
       <>
          {role ? (
             role !== "manager" ? (
-               <div className="mt-16">
+               <div className="mt-16 mt-16 w-full h-full">
                   <div className={"w-full flex flex-col items-center"}>
                      <BranchEditForm data={branch} />
                   </div>
