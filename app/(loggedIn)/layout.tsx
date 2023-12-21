00c764b4a4fbdc91a930749beaa7a21c8b414027
davit <kiladzedato5@gmail.com>
2023-12-21 23:54:18 +0400

    fix:socket

--- app/(loggedIn)/layout.tsx
+++ app/(loggedIn)/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import "/styles/globals.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSocket } from "@/contexts/SocketContext";
 import { renderSpecificToast } from "@/utils/renderSpecificToast";
 
@@ -10,11 +10,10 @@ export default function LoggedInRootLayout({
    children: React.ReactNode;
 }) {
    const { socket } = useSocket();
-   const [notification, setNotification] = useState<any>(null);
 
    useEffect(() => {
       const handleAlert = (message: any) => {
-         setNotification(message);
+         renderSpecificToast(message);
       };
 
       if (socket) {
@@ -22,10 +21,8 @@ export default function LoggedInRootLayout({
       }
 
       return () => {
-         setNotification(null);
          socket?.off("alert", handleAlert);
       };
    }, [socket]);
-   renderSpecificToast(notification);
    return <>{children}</>;
 }
