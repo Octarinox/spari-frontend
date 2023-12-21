b11a2020676064b7c836a72d0195c3c71ac9880d
davit <kiladzedato5@gmail.com>
2023-12-21 23:23:46 +0400

    fix:socket

--- app/(loggedIn)/(branchPage)/layout.tsx
+++ app/(loggedIn)/(branchPage)/layout.tsx
@@ -6,7 +6,6 @@ import { sideBarStyle } from "@/local-constants/styles/branchPageSidebar";
 import useBranchFetcher from "@/shared/hooks/useBranchFetch";
 import useUserFetcher from "@/shared/hooks/useUserFetch";
 import { useAuthActions } from "@/contexts/LoginContext/context";
-import { useEffect } from "react";
 
 export default function BranchPageLayout({
    children,
@@ -17,9 +16,7 @@ export default function BranchPageLayout({
    useUserFetcher();
    const { checkAuth } = useAuthActions();
 
-   useEffect(() => {
-      checkAuth();
-   }, [checkAuth]);
+   checkAuth();
    return (
       <main className={"flex flex-row"}>
          <Sidebar menu={branchPageSidebarItems} sideBarStyle={sideBarStyle} />
